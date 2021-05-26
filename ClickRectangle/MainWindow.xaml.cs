d2fe30ac94a9b156d90f25b7831361e5382ac8d7
Dmitry Naydenov <Vipdeem@gmail.com>
2021-05-26 21:26:47 +0300

    Добавьте файлы проекта.

---
+++ ClickRectangle/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClickRectangle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Brush customColor; // кисть 
+        Random r = new Random(); // рандом
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddOrRemoveItems(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is Rectangle)
+            {
+                Rectangle activeRectangle = (Rectangle)e.OriginalSource;
+
+                MyCanvas.Children.Remove(activeRectangle);
+            }
+            else
+            {
+                customColor = new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 255)));
+
+                Rectangle newRectangle = new Rectangle
+                {
+                    Width = 55,
+                    Height = 55,
+                    Fill = customColor,
+                    StrokeThickness = 3,
+                    Stroke = Brushes.Black
+                };
+
+                Canvas.SetLeft(newRectangle, Mouse.GetPosition(MyCanvas).X);
+                Canvas.SetTop(newRectangle, Mouse.GetPosition(MyCanvas).Y);
+
+                MyCanvas.Children.Add(newRectangle);
+            }
+
+        }
+    }
+}
